fa932919b87eb3aa82d0ee0559c065acb321f388
Shivam <shivam.s72@kalvium.community>
2025-03-05 16:42:40 +0530

    Initialized React app and created Landing Page for ASAP idea

---
+++ client/vite-project/src/LandingPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function LandingPage() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>Welcome to ASAP Idea</h1>
+      <p>ASAP is an innovative platform designed to streamline urgent tasks with efficiency and speed.</p>
+      <p>Our mission is to help users manage and complete their tasks as soon as possible with smart automation.</p>
+    </div>
+  );
+}
+
+export default LandingPage;
